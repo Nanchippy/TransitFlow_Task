315c12388eaa3d2498bb980fb48f5b2228a28ec9
Nandhana A Regi <nandhanaaregi@gmail.com>
2024-02-15 17:13:12 +0530

    misc changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+     <div className="App">
+     <header className='header'>
+      <div className='logoHeader'>
+        <h1>TransitFlow</h1>
+       <ul className='addressbar'>
+         <li>Mon - Sat 9.00 - 18.00<br/>
+             Sunday Closed
+         </li>
+         <li>Email<br/>
+            contact@logistics.com
+         </li>
+         <li>Call Us<br/>
+            (00) 112 365 489
+         </li>
+       </ul>
+       </div>
+
+      <nav>
+       <ul className='menu'>
+         <li>Home</li>
+       <li>About</li>
+         <li>Pages</li>
+         <li>Project</li>
+         <li>Contact</li>
+       </ul>
+       <ul className='content'>
+       <li>Instagram</li>
+       <li>Facebook</li>
+       <li>Twitter</li>
+       <li>linkedin</li>
+       </ul>
+       <button className='quote'>Request Quote</button>
+      </nav>
       </header>
-    </div>
+
+      <div className='banner'>
+<p className='log'>Logistics & Supply Chain Solutions</p>
+   <p className='gate'>Your Gateway <br/>
+   to any Destination in the 
+   <br/>World</p>
+   <p className='explanation'>
+   In augue ligula, feugiat ut nulla consequat. Ut est lacus, molestie in arcu no, iaculis <br/>vehicula ipsum. Nunc faucibus, nisl id dapibus finibus, enim diam interdum nulla, <br/>sed laoreet risus lectus.
+   </p>
+
+   <button className='explore'>Explore More</button>
+      </div>
+   
+
+     </div>
   );
 }
 
